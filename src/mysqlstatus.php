6f78ccae260bb72696ddb34a700c44a4c9eb2233
Oliver Jones <olliejones@gmail.com>
2021-10-28 10:51:09 -0400

    classify GLOBAL STATUS variables as accumulators or state variables

--- src/mysqlstatus.php
+++ src/mysqlstatus.php
@@ -1,23 +1,57 @@
-<?php
+<?php /** @noinspection SpellCheckingInspection */
 
 namespace olliejones\mysqlstatus;
 
-class MySQLStatus
-{
-    private $status;
-    private $vars;
-    private $timestamp;
-    public function __construct($status, $vars, $timestamp) {
-        $this->status = $status;
-        $this->vars = $vars;
-        $this->timestamp = $timestamp;
-    }
+require_once( 'cumulative.php' );
 
-    public function toString() {
-        return json_encode( array (
-            'timestamp' => $this->timestamp,
-            'status' => $this->status,
-            'vars' => $this->vars
-        ));
-    }
+class MySQLStatus {
+	private $status;
+	private $vars;
+	private $timestamp;
+	private $cumulatives;
+
+
+	public function __construct() {
+		$this->cumulatives = getCumulativeFilters();
+	}
+
+	public static function makeByArrays( $status, $variables, $timestamp ) {
+		$result            = new MySQLStatus();
+		$result->status    = $status;
+		$result->vars      = $variables;
+		$result->timestamp = $timestamp;
+
+		return $result;
+	}
+
+	public function toString() {
+		return json_encode( array(
+			'timestamp' => $this->timestamp,
+			'status'    => $this->status,
+			'vars'      => $this->vars
+		) );
+	}
+
+	public function show() {
+		$result   = array();
+		$mismatch = array();
+		foreach ( $this->status as $key => $value ) {
+			$found = false;
+			if ( is_numeric( $value ) ) {
+				foreach ( $this->cumulatives as $match ) {
+					$re = '/^' . $match . '$/i';
+					if ( preg_match( $re, $key ) === 1 ) {
+						$result[ $key ] = $value;
+						$found          = true;
+						break;
+					}
+				}
+			}
+			if ( ! $found ) {
+				$mismatch[ $key . '|mismatch' ] = $value;
+			}
+		}
+
+		return array_merge( $result, $mismatch );
+	}
 }
--- tests/test-mysqlstatus.php
+++ tests/test-mysqlstatus.php
@@ -1,10 +1,59 @@
-<?php
+<?php /** @noinspection SpellCheckingInspection */
 
 
-require_once ("../src/mysqlstatus.php");
-$testfileName = realpath('imfsstats.json');
-$stream  = fopen($testfileName, 'r');
-$data = json_decode(fread($stream, filesize($testfileName)), true);
-foreach ($data as $item) {
-    $k =  $item;
+require_once( "../src/mysqlstatus.php" );
+
+$cums        = array();
+$states      = array();
+$cumulatives = getCumulativeFilters();
+
+$testfileName = realpath( 'imfsstats.json' );
+$stream       = fopen( $testfileName, 'r' );
+$allData      = json_decode( fread( $stream, filesize( $testfileName ) ), true );
+fclose( $stream );
+
+foreach ( $allData as $item ) {
+	$data      = $item['data'];
+	$vars      = $data['variables'];
+	$status    = $data['globalStatus'];
+	$timestamp = $item['t'];
+
+	if (is_array($status)){
+		foreach ( $status as $key => $value ) {
+			$found = false;
+			if ( is_numeric( $value ) ) {
+				foreach ( $cumulatives as $match ) {
+					$re = '/^' . $match . '$/i';
+					if ( preg_match( $re, $key ) === 1 ) {
+						$found = true;
+						break;
+					}
+				}
+			}
+			if ( $found ) {
+				$a =  &$cums;
+			} else {
+				$a = &$states;
+			}
+			if ( ! array_key_exists( $key, $a ) ) {
+				$a[ $key ] = 0;
+			}
+			$a[ $key ] = $a [ $key ] + 1;
+
+		}	}
+}
+ksort($cums);
+ksort($states);
+
+
+if ( false ) {
+	foreach ( $allData as $item ) {
+		$data      = $item['data'];
+		$vars      = $data['variables'];
+		$status    = $data['globalStatus'];
+		$timestamp = $item['t'];
+		$a         = \olliejones\mysqlstatus\MySQLStatus::makeByArrays( $status, $vars, $timestamp );
+		$s         = $a->show();
+		print_r( $s );
+	}
 }
